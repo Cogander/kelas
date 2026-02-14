c506c380958a9d5594c9fac052a4c27f7b5d3885
Cogander <ivanderchristy13@gmail.com>
2026-02-14 15:32:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+function uid(){
+return Date.now().toString(36);
+}
+
 function login(){
 
 let role=document.getElementById("role").value;
@@ -5,15 +9,84 @@ let name=document.getElementById("name").value;
 let pass=document.getElementById("password").value;
 let info=document.getElementById("info");
 
-if(role==="guru" && pass!=="SmpXaverius1GuruHebat"){
+if(!name){
+info.innerText="Isi nama dulu";
+return;
+}
+
+
+/* ================= GURU ================= */
+
+if(role==="guru"){
+
+if(pass!=="SmpXaverius1GuruHebat"){
 info.innerText="Password Guru Salah";
 return;
 }
 
-if(role==="kepsek" && pass!=="SmpXaverius1KepalaHebat"){
+saveUser(role,name);
+return;
+}
+
+
+/* ================= KEPSEK ================= */
+
+if(role==="kepsek"){
+
+if(pass!=="SmpXaverius1KepalaHebat"){
 info.innerText="Password Salah";
 return;
 }
+
+saveUser(role,name);
+return;
+}
+
+
+/* ================= SISWA ================= */
+
+let students=DB.get("students");
+
+let existing=students.find(s=>s.name===name);
+
+
+if(existing){
+
+// login normal
+if(existing.password!==pass){
+info.innerText="Password salah";
+return;
+}
+
+saveUser(role,name);
+return;
+
+}else{
+
+// siswa baru → buat password
+if(!pass){
+info.innerText="Buat password dulu";
+return;
+}
+
+students.push({
+name:name,
+password:pass
+});
+
+DB.set("students",students);
+
+saveUser(role,name);
+return;
+
+}
+
+
+}
+
+
+
+function saveUser(role,name){
 
 let user={
 id:uid(),
